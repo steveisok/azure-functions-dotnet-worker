385577cd90a6e4cd4f6ffd53644221d05a724317
Ivan Diaz <ivdiazsa@microsoft.com>
2024-04-24 16:03:49 -0700

    Hook App written. Now, only the Startup Hook remains to be added before we can focus on testing.

---
+++ samples/HookApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello from the Hook App! If you got here, then you made"
+                          + " a wrong turn along the way. Try to retrace your steps"
+                          + " and try again.");
+    }
+}
