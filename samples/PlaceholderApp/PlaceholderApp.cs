28758d2517e7491b8d2ea9b367e5b06736d9017e
Ivan Diaz <ivdiazsa@microsoft.com>
2024-04-29 14:13:01 -0700

    Tweaked the Startup Hook and Dummy App to run the final app using Reflection.

--- samples/PlaceholderApp/PlaceholderApp.cs
+++ samples/PlaceholderApp/PlaceholderApp.cs
@@ -1,12 +1,34 @@
 using System;
+using System.IO;
+using System.Reflection;
+
+using static System.Environment;
 
 public class PlaceholderApp
 {
+    const string WorkerLogFileEnvVar = "AZURE_FUNCTIONS_WORKER_LOGFILE_PATH";
+
     static int Main(string[] args)
     {
-        Console.WriteLine("Hello from Placeholder App! If you got here, then you"
-                          + " made a wrong turn along the way. Retrace your steps"
-                          + " and try again. Good luck!");
-        return 0;
+        // Console.WriteLine("Hello from Placeholder App! If you got here, then you"
+        //                   + " made a wrong turn along the way. Retrace your steps"
+        //                   + " and try again. Good luck!");
+
+        Assembly entry = Assembly.GetEntryAssembly();
+        LogMessage($"Going to execute the entry assembly {entry.FullName}...");
+
+        AppDomain thisDomain = AppDomain.CurrentDomain;
+        int exitCode = thisDomain.ExecuteAssemblyByName(entry.GetName());
+        return exitCode;
+    }
+
+    private static void LogMessage(string message)
+    {
+        string logFile = GetEnvironmentVariable(WorkerLogFileEnvVar);
+
+        if (string.IsNullOrWhiteSpace(logFile))
+            Console.WriteLine($"PLACEHOLDER CONSOLE: {message}");
+        else
+            File.AppendAllTextAsync(logFile, $"{message}{NewLine}");
     }
 }
